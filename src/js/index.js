7f23cceebbc1b0e502888ee5eaaca72cc363785d
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-14 00:39:02 +0300

    Добавил изменение темы

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,26 @@ import PhotosApiService from './apiService';
 const formEl = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-button');
-
-
-
+const toggleEl = document.querySelector('.toggle-checkbox');
 
 
 formEl.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
+toggleEl.addEventListener('click', changeThemeOnClick);
+
+
+function changeThemeOnClick() {
+  if (galleryEl.classList.contains('dark')) {
+    galleryEl.classList.remove('dark');
+    galleryEl.classList.add('light');
+    localStorage.setItem('theme', 'light');
+  } else {
+    galleryEl.classList.remove('light');
+    galleryEl.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
 
 const photosApiService = new PhotosApiService();
 
@@ -27,12 +40,11 @@ function onSearch(event) {
 
 function onLoadMore() {
   photosApiService.fetchPhotos().then(renderPhotosGallery);
-  // loadMoreBtn.scrollIntoView({block: "end", behavior: "smooth"});
 }
 
 function renderPhotosGallery(photo) {
   const markup = photoCardTpl(photo);
-  galleryEl.classList.add('is-active');
+  galleryEl.classList.add('light');
   galleryEl.insertAdjacentHTML('beforeend', markup);
   loadMoreBtn.classList.add('is-visible');
 }
